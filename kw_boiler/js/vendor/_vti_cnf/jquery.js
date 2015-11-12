15e778c8076992de596e95fdda5f58c770cdadf8
mdlawrenceusa <mdlawrence@me.com>
2015-11-12 17:41:58 -0500

    end of class

--- kw_boiler/js/vendor/_vti_cnf/jquery.js
+++ kw_boiler/js/vendor/_vti_cnf/jquery.js
@@ -1,9 +1,9 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|ET\\Instructor
 vti_modifiedby:SR|ET\\Instructor
-vti_timelastmodified:TR|22 Oct 2015 19:08:39 -0000
+vti_timelastmodified:TR|12 Nov 2015 19:34:41 -0000
 vti_timecreated:TR|17 Sep 2015 20:52:51 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|master.dwt libraries/default.html directory/default.html libraries/research/research.html libraries/services/services.html default.html
-vti_cacheddtm:TX|22 Oct 2015 19:08:39 -0000
+vti_backlinkinfo:VX|master.dwt libraries/default.html directory/default.html libraries/research/research.html libraries/services/services.html default.html rusebk/default.html
+vti_cacheddtm:TX|12 Nov 2015 19:34:41 -0000
 vti_filesize:IR|84759
